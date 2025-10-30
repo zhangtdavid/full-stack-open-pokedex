e9e5990e22df1ff5c6416bee9d3dce099fea476e
zhangtdavid <zhangtdavid@gmail.com>
2025-10-30 10:40:27 -0700

    11.9

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,9 @@
+import { test, describe, expect } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
